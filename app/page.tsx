e69b8c778e3abe4eb208245884b13993381ccef9
fazbello <156936462+fazbello@users.noreply.github.com>
2025-09-26 09:12:53 +0100

    Change 'Bolt Swap' to 'Thunda Swap' in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@
 
 export default function Home() {
   return (
-        <main className="absolute flex flex-row justify-center items-center h-[90vh] w-[90vw] text-white text-[15rem]">
-          Bolt Swap
-        </main>
-    
-
+    <main className="absolute flex flex-row justify-center items-center h-[90vh] w-[90vw] text-white text-[15rem]">
+      Thunda Swap
+    </main>
   );
 }
